f8bb086f58d305fe72e039baefe2612f9078709b
immonica <imonicaanamaria@gmail.com>
2023-12-21 15:57:36 +0200

    Sign In + Sign Up Actions

--- app/src/main/java/com/example/todoorganizer/fragments/SigninFragment.java
+++ app/src/main/java/com/example/todoorganizer/fragments/SigninFragment.java
@@ -1,66 +1,74 @@
 package com.example.todoorganizer.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.todoorganizer.R;
+import com.example.todoorganizer.databinding.FragmentSigninBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SigninFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SigninFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SigninFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SigninFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SigninFragment newInstance(String param1, String param2) {
-        SigninFragment fragment = new SigninFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private NavController navController;
+    private FirebaseAuth mAuth;
+    private FragmentSigninBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false);
+        binding = FragmentSigninBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+
+        binding.textViewSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_signinFragment_to_signupFragment);
+            }
+        });
+
+        binding.nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.emailEt.getText().toString();
+                String pass = binding.passEt.getText().toString();
+
+                if (!email.isEmpty() && !pass.isEmpty()) {
+                    loginUser(email, pass);
+                } else {
+                    Toast.makeText(requireContext(), "Empty fields are not allowed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void loginUser(String email, String pass) {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                navController.navigate(R.id.action_signinFragment_to_homeFragment);
+            } else {
+                Toast.makeText(requireContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void init(View view) {
+        navController = Navigation.findNavController(view);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
--- app/src/main/java/com/example/todoorganizer/fragments/SignupFragment.java
+++ app/src/main/java/com/example/todoorganizer/fragments/SignupFragment.java
@@ -4,57 +4,76 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.todoorganizer.R;
+import com.example.todoorganizer.databinding.FragmentSignupBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SignupFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignupFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignupFragment newInstance(String param1, String param2) {
-        SignupFragment fragment = new SignupFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private NavController navController;
+    private FirebaseAuth mAuth;
+    private FragmentSignupBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+        binding = FragmentSignupBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+
+        binding.textViewSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_signupFragment_to_signinFragment);
+            }
+        });
+
+        binding.nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.emailEt.getText().toString();
+                String pass = binding.passEt.getText().toString();
+                String verifyPass = binding.verifyPassEt.getText().toString();
+
+                if (!email.isEmpty() && !pass.isEmpty() && !verifyPass.isEmpty()) {
+                    if (pass.equals(verifyPass)) {
+                        registerUser(email, pass);
+                    } else {
+                        Toast.makeText(requireContext(), "Password is not the same", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Empty fields are not allowed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String pass) {
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                navController.navigate(R.id.action_signupFragment_to_homeFragment);
+            } else {
+                Toast.makeText(requireContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void init(View view) {
+        navController = Navigation.findNavController(view);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
